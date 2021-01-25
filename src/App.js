4848212d9373e389cbff115c7981acfdf6162c7c
Mujhtech <mujeeb.muhideen@gmail.com>
2021-01-25 09:19:41 +0100

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,53 @@
-import './App.css';
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Card from './components/ui/Card';
-import Search from './components/ui/Search'
-import Header from './components/ui/Header'
-import Pagination from './components/ui/Pagination';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "./components/ui/Card";
+import Search from "./components/ui/Search";
+import Header from "./components/ui/Header";
+import Pagination from "./components/ui/Pagination";
 //import Filter from './components/ui/Filter';
 
 function App() {
-
-  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [query, setQuery] = useState('')
-  const [currentPage, setCurrentPage] = useState(1)
-  const [profilePerPage] = useState(20)
-
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [profilePerPage] = useState(20);
 
   useEffect(() => {
-    const fetchItems = async () => {
-      setIsLoading(true)
-      const result = await axios(
-        `https://api.enye.tech/v1/challenge/records`
-      )
-      setItems(result.data.records.profiles)
-      setIsLoading(false)
-    }
+    const fetchItems = async (input) => {
+      setIsLoading(true);
+      const result = await axios(`https://api.enye.tech/v1/challenge/records`);
+      if (input !== "") {
+        const filtered = result.data.records.profiles.filter((profile) => {
+          return profile.FirstName.toLowerCase().includes(input.toLowerCase());
+        });
+        setItems(filtered);
+      } else {
+        setItems(result.data.records.profiles);
+      }
+      setIsLoading(false);
+    };
 
-    fetchItems()
-  }, [])
+    fetchItems(query);
+  }, [query]);
 
-  const indexOfLastProfile = currentPage * profilePerPage
-  const indexOfFirstProfile = indexOfLastProfile - profilePerPage
-  const currentProfile = items.slice(indexOfFirstProfile, indexOfLastProfile)
-  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+  const indexOfLastProfile = currentPage * profilePerPage;
+  const indexOfFirstProfile = indexOfLastProfile - profilePerPage;
+  const currentProfile = items.slice(indexOfFirstProfile, indexOfLastProfile);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div className="container">
       <Header />
       {/* <Filter /> */}
       <Search getQuery={(q) => setQuery(q)} />
-      <Card isLoading={isLoading} items={currentProfile} />
-      <Pagination profilePerPage={profilePerPage} totalProfiles={items.length} paginate={paginate}/>
+      <Card isLoading={isLoading} items={currentProfile} query={query} />
+      <Pagination
+        profilePerPage={profilePerPage}
+        totalProfiles={items.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -2,14 +2,19 @@ import React from 'react'
 import CardItem from './CardItem'
 import Spinner from './Spinner'
 
-export default function Card({isLoading, items}) {
-    return isLoading ? (
-        <Spinner />
-      ) : (
-        <section className='cards'>
-          {items.map((item, index) => (
-            <CardItem key={index} item={item}></CardItem>
-          ))}
-        </section>
-      )
+export default function Card({isLoading, items, query}) {
+
+    if(isLoading){
+      return (<Spinner />)
+    } else {
+      
+        return (
+          <section className='cards'>
+            {items.map((item, index) => (
+              <CardItem key={index} item={item}></CardItem>
+            ))}
+          </section>
+        )
+
+    }
 }
